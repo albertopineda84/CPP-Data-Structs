1ed0a33f9e77c3b8d9641ed9afc5445902466ff0
albertopineda84 <albertopineda84@gmail.com>
2020-07-27 23:11:43 -0700

    Add files via upload

---
+++ XBoxGame.h
@@ -0,0 +1,6 @@
+#pragma once
+class XBoxGame
+{
+public:
+	void MakeNoise() {};
+};
--- main.cpp
+++ main.cpp
@@ -1,35 +1,12 @@
-#include "FakeInt.h"
-
-#include <iostream>
-using namespace std;
+#include "Gift.h"
+#include "XBoxGame.h"
+#include "WorldPeace.h"
 
 int main()
 {
-	int tMyNum = 1;
+	Gift<XBoxGame> skyrim;
+	Gift<WorldPeace> imaginary;
 
-	while (tMyNum > 0)
-	{
-		cout << "Please enter a number less than " << INT_MAX << " to return its word form." << endl;
-		cout << "Otherwise, enter a number less than 1 to quit: ";
-		cin >> tMyNum;
-
-		while (tMyNum > INT_MAX)
-		{
-			cerr << "Error! You have entered an invalid value. Please enter a lower number: ";
-			cin >> tMyNum;
-		}
-
-		if (tMyNum <= 0)
-			break;
-
-		FakeInt ThisInt(tMyNum);
-
-		cout << ThisInt.IntWord() << endl;
-
-
-	}
-
-	system("pause");
-
+	skyrim.Shake();
 	return 0;
 }
